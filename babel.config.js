87ff092baa93f0190fe0327eea96bc621cf2ba1b
dhiyo7 <dhiyoreksa12@gmail.com>
2021-11-20 17:08:50 +0700

    initial project

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports={
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins:[
+        [
+            'module-resolver',
+            {
+                root:['./src'],
+                alias:{
+                    '@src':'./src',
+                    '@assets': './src/assets',
+                    '@components': './src/components',
+                    '@pages': './src/pages',
+                }
+            }
+        ]
+    ]
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navbar } from "../../components/molecules";
+
+const Home = () => {
+  return (
+    <Navbar/>
+  );
+};
+
+export default Home;
